71b8df4120d007b338b4c32fa759284012f4c2c6
Matthias Robbers <matthias.robbers@gmail.com>
2012-04-30 20:44:25 +0000

    prepared LocationActivity ("nearby"), it now gets the device's location using the network provider information and prints out city name, country name, latitude and longitude
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@15 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/de/ifgi/fmt/activities/LocationActivity.java
+++ src/de/ifgi/fmt/activities/LocationActivity.java
@@ -1,21 +1,83 @@
 package de.ifgi.fmt.activities;
 
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 import de.ifgi.fmt.R;
 
-import android.content.Intent;
-import android.os.Bundle;
-
 public class LocationActivity extends SherlockActivity {
-	/** Called when the activity is first created. */
+	LocationManager locationManager;
+	LocationListener locationListener;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.location_activity);
 
 		getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// Acquire a reference to the system Location Manager
+		locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+
+		// Define a listener that responds to location updates
+		locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location
+				// provider.
+				LinearLayout waitingForLocation = (LinearLayout) findViewById(R.id.waiting_for_location);
+				waitingForLocation.setVisibility(View.GONE);
+
+				Geocoder geocoder = new Geocoder(getApplicationContext());
+				List<Address> addresses = null;
+				try {
+					addresses = geocoder.getFromLocation(
+							location.getLatitude(), location.getLongitude(), 1);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				Address address = addresses.get(0);
+
+				TextView addressText = (TextView) findViewById(R.id.address_text);
+				TextView locationText = (TextView) findViewById(R.id.location_text);
+				addressText.setText(address.getLocality() + ", "
+						+ address.getCountryName());
+				locationText.setText("Latitude: " + location.getLatitude()
+						+ "\n" + "Longitude: " + location.getLongitude());
+				// Remove the listener
+				locationManager.removeUpdates(locationListener);
+			}
+
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
+
+			public void onProviderEnabled(String provider) {
+			}
+
+			public void onProviderDisabled(String provider) {
+			}
+		};
+
+		// Register the listener with the Location Manager to receive location
+		// updates
+		locationManager.requestLocationUpdates(
+				LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 	}
 
 	@Override
