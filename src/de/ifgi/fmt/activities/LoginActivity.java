a480ca8f54a0d4da041f3e9de592dada8f4cfb5d
Matthias Robbers <matthias.robbers@gmail.com>
2012-05-01 16:25:52 +0000

    Added login/logout, ready for use with engine
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@16 471a90e3-85d6-13bc-70e2-46c3232741f2

---
+++ src/de/ifgi/fmt/activities/LoginActivity.java
@@ -0,0 +1,146 @@
+package de.ifgi.fmt.activities;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import de.ifgi.fmt.R;
+
+public class LoginActivity extends SherlockActivity {
+	public static final int STATUS_NOT_VALID = 12;
+	public static final int STATUS_OK = 11;
+
+	public static final int REDIRECT_TO_ACTIVITY_1 = 1;
+	public static final int REDIRECT_TO_ACTIVITY_2 = 2;
+	// ...
+
+	private EditText username, password;
+	private Button login;
+	private ProgressDialog progressDialog;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity);
+		setTitle("Login");
+		getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
+		username = (EditText) findViewById(R.id.username);
+
+		// already in XML, specially for HTC in Java
+		username.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+		password = (EditText) findViewById(R.id.password);
+		password.setOnEditorActionListener(new OnEditorActionListener() {
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				if (actionId == 0) {
+					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(password.getWindowToken(), 0);
+					login.performClick();
+				}
+				return false;
+			}
+		});
+
+		login = (Button) findViewById(R.id.login);
+		login.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				authenticate();
+			}
+		});
+
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage("Authenticating...");
+	}
+
+	private void authenticate() {
+		if (username.getText().toString().equals("")
+				|| password.getText().toString().equals("")) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Missing data");
+			builder.setIcon(android.R.drawable.ic_dialog_alert);
+			builder.setMessage("Please enter your user name and password.")
+					.setNeutralButton("OK",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									dialog.cancel();
+								}
+							});
+			builder.create().show();
+		} else {
+			progressDialog.show();
+			new Thread() {
+				public void run() {
+
+					// Login request (server) goes here...
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					handler.sendEmptyMessage(0);
+
+				};
+
+				private Handler handler = new Handler() {
+					@Override
+					public void handleMessage(Message msg) {
+						// work with the result
+						switch (msg.what) {
+						default:
+							SharedPreferences preferences = PreferenceManager
+									.getDefaultSharedPreferences(getApplicationContext());
+							SharedPreferences.Editor editor = preferences
+									.edit();
+							editor.putInt("user_id", 123);
+							editor.putString("user_name", "markymark");
+							editor.putString("user_email",
+									"wahlberg@hollywood.com");
+							editor.commit();
+							break;
+						}
+						progressDialog.dismiss();
+						redirect();
+					}
+				};
+			}.start();
+		}
+	}
+
+	public void redirect() {
+		Intent intent = null;
+		switch (getIntent().getExtras().getInt("startActivity")) {
+		case REDIRECT_TO_ACTIVITY_1:
+			intent = new Intent(this, StartActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			break;
+		case REDIRECT_TO_ACTIVITY_2:
+			// ...
+			break;
+		}
+		startActivity(intent);
+		Toast.makeText(this, "Welcome", Toast.LENGTH_LONG).show();
+		finish();
+	}
+
+}
--- src/de/ifgi/fmt/activities/StartActivity.java
+++ src/de/ifgi/fmt/activities/StartActivity.java
@@ -1,41 +1,75 @@
 package de.ifgi.fmt.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
 import de.ifgi.fmt.R;
 
-public class StartActivity extends SherlockActivity
-{
-	/** Called when the activity is first created. */
+public class StartActivity extends SherlockActivity {
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_activity);
 	}
 
-	public void startMapActivity(View v)
-	{
+	@Override
+	protected void onResume() {
+		super.onResume();
+		updateLoginLogoutButton();
+	}
+
+	public void startMapActivity(View v) {
 		startActivity(new Intent(this, MapActivity.class));
 	}
 
-	public void startLocationActivity(View v)
-	{
+	public void startLocationActivity(View v) {
 		startActivity(new Intent(this, LocationActivity.class));
 	}
 
-	public void startNameActivity(View v)
-	{
-//		startActivity(new Intent(this, NameActivity.class));
+	public void startNameActivity(View v) {
+		// startActivity(new Intent(this, NameActivity.class));
 		startActivity(new Intent(this, FlashmobListActivity.class));
 	}
 
-	public void startAttributesActivity(View v)
-	{
+	public void startAttributesActivity(View v) {
 		startActivity(new Intent(this, AttributesActivity.class));
+	}
+
+	public void startLoginActivity(View v) {
+		startActivity(new Intent(this, LoginActivity.class).putExtra(
+				"startActivity", LoginActivity.REDIRECT_TO_ACTIVITY_1));
+	}
+
+	public void startLogoutActivity(View v) {
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.remove("user_id");
+		editor.remove("user_name");
+		editor.remove("user_email");
+		editor.commit();
+		updateLoginLogoutButton();
+	}
+
+	private void updateLoginLogoutButton() {
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		if (preferences.getInt("user_id", 0) == 0) {
+			((LinearLayout) findViewById(R.id.login_button))
+					.setVisibility(View.VISIBLE);
+			((LinearLayout) findViewById(R.id.logout_button))
+					.setVisibility(View.GONE);
+		} else {
+			((LinearLayout) findViewById(R.id.login_button))
+					.setVisibility(View.GONE);
+			((LinearLayout) findViewById(R.id.logout_button))
+					.setVisibility(View.VISIBLE);
+		}
 	}
 }
