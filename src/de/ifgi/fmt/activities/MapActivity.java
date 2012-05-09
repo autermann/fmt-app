0e958fb5529f3e3a405d35c25b32cce6d99fa654
Stefan Arndt <stefan.arndt86@gmail.com>
2012-05-09 00:00:10 +0000

    Map: Flashmob-Infos in PopUp and new layout
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@27 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/de/ifgi/fmt/activities/MapActivity.java
+++ src/de/ifgi/fmt/activities/MapActivity.java
@@ -1,10 +1,9 @@
 package de.ifgi.fmt.activities;
 
-//import android.app.AlertDialog;
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+
 
 import com.actionbarsherlock.app.SherlockMapActivity;
 import com.actionbarsherlock.view.Menu;
@@ -20,11 +19,18 @@ import android.graphics.drawable.Drawable;
 import java.util.ArrayList;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MyLocationOverlay;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import de.ifgi.fmt.R;
 
-public class MapActivity extends SherlockMapActivity
-{
+public class MapActivity extends SherlockMapActivity {
 	private static final int MENU_LAYER_MAP = 1;
 	private static final int MENU_LAYER_SATELLITE = 2;
 
@@ -32,19 +38,18 @@ public class MapActivity extends SherlockMapActivity
 	MapController mc;
 	GeoPoint p;
 	private MyLocationOverlay me = null;
-	public Context mContext;
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_activity);
 		getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
 		mapView = (MapView) findViewById(R.id.mapview);
 		Drawable marker = getResources().getDrawable(R.drawable.location);
 
-		marker.setBounds(0, 0, marker.getIntrinsicWidth(), marker.getIntrinsicHeight());
+		marker.setBounds(0, 0, marker.getIntrinsicWidth(),
+				marker.getIntrinsicHeight());
 
 		mapView.getOverlays().add(new SitesOverlay(marker));
 
@@ -62,13 +67,13 @@ public class MapActivity extends SherlockMapActivity
 		mc.animateTo(p);
 		mc.setZoom(13);
 		mapView.invalidate();
+		
+		
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case android.R.id.home:
 			// app icon in action bar clicked; go home
 			Intent intent = new Intent(this, StartActivity.class);
@@ -86,20 +91,29 @@ public class MapActivity extends SherlockMapActivity
 		}
 	}
 
-	private GeoPoint getPoint(double lat, double lon)
-	{
+	private GeoPoint getPoint(double lat, double lon) {
 		return (new GeoPoint((int) (lat * 1000000.0), (int) (lon * 1000000.0)));
 	}
 
 	@Override
-	protected boolean isRouteDisplayed()
-	{
+	protected boolean isRouteDisplayed() {
 		return false;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public void onResume() {
+		super.onResume();
+		me.enableCompass();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		me.disableCompass();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		SubMenu layers = menu.addSubMenu("Layers");
 		layers.add(0, MENU_LAYER_MAP, 0, "Map");
 		layers.add(0, MENU_LAYER_SATELLITE, 0, "Satellite");
@@ -112,61 +126,133 @@ public class MapActivity extends SherlockMapActivity
 		return super.onCreateOptionsMenu(menu);
 	}
 
-	private class SitesOverlay extends ItemizedOverlay<OverlayItem>
-	{
+	private class SitesOverlay extends ItemizedOverlay<OverlayItem> {
 		private List<OverlayItem> items = new ArrayList<OverlayItem>();
+		private Drawable marker = null;
+		private PopupPanel panel = new PopupPanel(R.layout.popup);
 
-		public SitesOverlay(Drawable marker)
-		{
+		public SitesOverlay(Drawable marker) {
 			super(marker);
-
+			this.marker=marker; 
+			
 			boundCenterBottom(marker);
 
 			// List of Points (FMs) to display
-			items.add(new OverlayItem(getPoint(51.940932, 7.609992), "Freeze Flashmob",
-					" Come here on 23.05.2012"));
-			items.add(new OverlayItem(getPoint(51.951195, 7.603297), "Freeze Flashmob",
-					"Starting on 24.05.2012"));
-			items.add(new OverlayItem(getPoint(51.962409, 7.631621), "Pillow Flashmob",
-					"Party @ 25.05.2012"));
-			items.add(new OverlayItem(getPoint(51.963995, 7.610507), "Pillow Flashmob",
-					"Let's meet: 26.05.2012"));
+			items.add(new OverlayItem(getPoint(51.940932, 7.609992),
+					"Freeze Flashmob", " Come here on 23.05.2012"));
+			items.add(new OverlayItem(getPoint(51.951195, 7.603297),
+					"Freeze Flashmob", "Starting on 24.05.2012"));
+			items.add(new OverlayItem(getPoint(51.962409, 7.631621),
+					"Pillow Flashmob", "Party @ 25.05.2012"));
+			items.add(new OverlayItem(getPoint(51.963995, 7.610507),
+					"Pillow Flashmob", "Let's meet: 26.05.2012"));
 
 			populate();
 		}
 
 		@Override
-		protected OverlayItem createItem(int i)
-		{
+		protected OverlayItem createItem(int i) {
 			return (items.get(i));
 		}
 
-		// Traditional way of displaying the information with a AlertDialog
-		// @Override
-		// protected boolean onTap(int i) {
-		// OverlayItem item = items.get(i);
-		// AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-		// dialog.setTitle(item.getTitle());
-		// dialog.setMessage(item.getSnippet());
-		// dialog.show();
-		// return true;
-		// }
-
-		// Cool Way of displaying the information of the FM
 		@Override
-		protected boolean onTap(int i)
-		{
-			Toast.makeText(MapActivity.this,
-					items.get(i).getTitle() + "\n" + items.get(i).getSnippet(), Toast.LENGTH_LONG)
-					.show();
+		public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+			super.draw(canvas, mapView, shadow);
+
+			boundCenterBottom(marker);
+		}
+		
+		
+		
+		
+		@Override
+		protected boolean onTap(int i) {
+			OverlayItem item = getItem(i);
+			GeoPoint geo = item.getPoint();
+			Point pt = mapView.getProjection().toPixels(geo, null);
+
+			View view = panel.getView();
+			 
+            ((TextView) view.findViewById(R.id.title)).setText(String.valueOf(item.getTitle()));
+            ((TextView) view.findViewById(R.id.description)).setText(String.valueOf(item.getSnippet()));
+            
+            Button more = (Button) view.findViewById(R.id.more);
+            more.setOnClickListener(new OnClickListener() {
+            	public void onClick(View v) {
+            			System.out.println("YAHOOOOOOO ");
+            			//funktioniert!            
+            			}
+            	});
+            
+            
+            
+			panel.show(pt.y * 2 > mapView.getHeight());
+			
+			
 
 			return (true);
 		}
 
 		@Override
-		public int size()
-		{
+		public int size() {
 			return (items.size());
+		}
+		
+	
+	}
+
+	class PopupPanel {
+		View popup;
+		boolean isVisible = false;
+
+		PopupPanel(int layout) {
+			ViewGroup parent = (ViewGroup) mapView.getParent();
+
+			popup = getLayoutInflater().inflate(layout, parent, false);
+
+			popup.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					hide();
+				}
+			});
+			
+			mapView.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					isVisible = false;
+					((ViewGroup) popup.getParent()).removeView(popup);			
+				}
+			});
+			
+		}
+		
+		View getView() {
+			return (popup);
+		}
+
+		void show(boolean alignTop) {
+			RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT,
+					RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+			if (alignTop) {
+				lp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+				lp.setMargins(0, 20, 0, 0);
+			} else {
+				lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+				lp.setMargins(0, 0, 0, 60);
+			}
+
+			hide();
+
+			((ViewGroup) mapView.getParent()).addView(popup, lp);
+			isVisible = true;
+		}
+
+		void hide() {
+			if (isVisible) {
+				isVisible = false;
+				((ViewGroup) popup.getParent()).removeView(popup);
+			}
 		}
 	}
 
