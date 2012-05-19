c27fc2e1e7a64bf16e0ebc913b9c2be455d6168c
Matthias Robbers <matthias.robbers@gmail.com>
2012-05-19 16:41:22 +0000

    re-added login and logout as action bar icons
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@79 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/de/ifgi/fmt/activities/StartActivity.java
+++ src/de/ifgi/fmt/activities/StartActivity.java
@@ -7,10 +7,15 @@ import android.preference.PreferenceManager;
 import android.view.View;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 import de.ifgi.fmt.R;
 
 public class StartActivity extends SherlockActivity {
+	private static final int MENU_LOGIN = 1;
+	private static final int MENU_LOGOUT = 2;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,7 +25,7 @@ public class StartActivity extends SherlockActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		updateLoginLogoutButton();
+		invalidateOptionsMenu();
 	}
 
 	public void startMapActivity(View v) {
@@ -39,38 +44,45 @@ public class StartActivity extends SherlockActivity {
 		startActivity(new Intent(this, AttributesActivity.class));
 	}
 
-	public void startLoginActivity(View v) {
-		startActivity(new Intent(this, LoginActivity.class).putExtra(
-				"startActivity", LoginActivity.REDIRECT_TO_ACTIVITY_1));
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case MENU_LOGIN:
+			startActivity(new Intent(this, LoginActivity.class).putExtra(
+					"startActivity", LoginActivity.REDIRECT_TO_ACTIVITY_1));
+			break;
+		case MENU_LOGOUT:
+			SharedPreferences preferences = PreferenceManager
+					.getDefaultSharedPreferences(getApplicationContext());
+			SharedPreferences.Editor editor = preferences.edit();
+			editor.remove("user_id");
+			editor.remove("user_name");
+			editor.remove("user_email");
+			editor.commit();
+			invalidateOptionsMenu();
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
-	public void startLogoutActivity(View v) {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		SharedPreferences preferences = PreferenceManager
 				.getDefaultSharedPreferences(getApplicationContext());
-		SharedPreferences.Editor editor = preferences.edit();
-		editor.remove("user_id");
-		editor.remove("user_name");
-		editor.remove("user_email");
-		editor.commit();
-		updateLoginLogoutButton();
-	}
-
-	private void updateLoginLogoutButton() {
-		SharedPreferences preferences = PreferenceManager
-				.getDefaultSharedPreferences(getApplicationContext());
-		// if (preferences.getInt("user_id", 0) == 0)
-		// {
-		// ((LinearLayout)
-		// findViewById(R.id.login_button)).setVisibility(View.VISIBLE);
-		// ((LinearLayout)
-		// findViewById(R.id.logout_button)).setVisibility(View.GONE);
-		// }
-		// else
-		// {
-		// ((LinearLayout)
-		// findViewById(R.id.login_button)).setVisibility(View.GONE);
-		// ((LinearLayout)
-		// findViewById(R.id.logout_button)).setVisibility(View.VISIBLE);
-		// }
+		if (preferences.getInt("user_id", 0) == 0) {
+			menu.add(0, MENU_LOGIN, 1, "Login")
+					.setIcon(R.drawable.ic_login)
+					.setShowAsAction(
+							MenuItem.SHOW_AS_ACTION_ALWAYS
+									| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		} else {
+			menu.add(0, MENU_LOGOUT, 1, "Logout")
+					.setIcon(R.drawable.ic_logout)
+					.setShowAsAction(
+							MenuItem.SHOW_AS_ACTION_ALWAYS
+									| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		}
+		return super.onCreateOptionsMenu(menu);
 	}
 }
