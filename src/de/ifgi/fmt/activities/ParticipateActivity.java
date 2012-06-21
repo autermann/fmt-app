98645c71a8e68db23d9ed6e306fe77b36e7a1173
Matthias Robbers <matthias.robbers@gmail.com>
2012-06-21 11:24:24 +0000

    new layout for ParticpateActivity
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@156 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/de/ifgi/fmt/activities/ParticipateActivity.java
+++ src/de/ifgi/fmt/activities/ParticipateActivity.java
@@ -1,10 +1,12 @@
 package de.ifgi.fmt.activities;
 
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,28 +17,42 @@ import de.ifgi.fmt.R;
 import de.ifgi.fmt.data.Store;
 import de.ifgi.fmt.objects.Flashmob;
 
-public class ParticipateActivity extends SherlockActivity
-{
+public class ParticipateActivity extends SherlockActivity {
 	private Button participateButton;
 	private SharedPreferences prefs;
 	private Flashmob flashmob;
 	private String id;
 	private String PARTICIPATION_PREF_KEY;
-	private TextView roleDescriptionTv;
 	private Spinner roleSpinner;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.participate_activity);
-
+		
+		LinearLayout rolesList = (LinearLayout) findViewById(R.id.roles_list);
+		TextView roleTitle;
+		TextView roleDescription;
+		
+		for (int i = 0; i < 3; i++) {
+			roleTitle = new TextView(this);
+			roleTitle.setTypeface(Typeface.DEFAULT_BOLD);
+			roleTitle.setTextSize(16);
+			roleTitle.setText("Title");
+			roleDescription = new TextView(this);
+			roleDescription.setPadding(0, 0, 0, 16);
+			roleDescription
+					.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ");
+			rolesList.addView(roleTitle);
+			rolesList.addView(roleDescription);
+		}
 		getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		// Initialize id
 		id = getFlashmobID();
 
-		// This is a unique key string for saving the user's participation status
+		// This is a unique key string for saving the user's participation
+		// status
 		PARTICIPATION_PREF_KEY = id + "Pref";
 
 		// Initialize SharedPreferences
@@ -47,57 +63,52 @@ public class ParticipateActivity extends SherlockActivity
 
 		setTitle(flashmob.getTitle());
 
-		// Role description TextView
-		roleDescriptionTv = (TextView) findViewById(R.id.roleDescriptionTV);
-
 		// Role Spinner
 		roleSpinner = (Spinner) findViewById(R.id.roleSpinner);
 		// TODO: Add adapter!!!
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-				R.array.roleDummyArray, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.roleDummyArray,
+				android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		roleSpinner.setAdapter(adapter);
-		
+
 		// Participate button
 		participateButton = (Button) findViewById(R.id.participateButton);
-		participateButton.setOnClickListener(new View.OnClickListener()
-		{
+		participateButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
-				boolean isParticipating = prefs.getBoolean(PARTICIPATION_PREF_KEY, false);
+			public void onClick(View v) {
+				boolean isParticipating = prefs.getBoolean(
+						PARTICIPATION_PREF_KEY, false);
 
 				final SharedPreferences.Editor editor = prefs.edit();
 
 				// Change button text depending on participation
-				if (!isParticipating)
-				{
+				if (!isParticipating) {
 					editor.putBoolean(PARTICIPATION_PREF_KEY, true);
 					editor.commit();
 					participateButton.setText("Cancel Participation");
-					participateButton.setBackgroundResource(R.drawable.cancel_button_background);
+					participateButton
+							.setBackgroundResource(R.drawable.cancel_button_background);
 
 					// TODO: Participate-Funktion hinzufügen
-				}
-				else
-				{
+				} else {
 					editor.putBoolean(PARTICIPATION_PREF_KEY, false);
 					editor.commit();
 					participateButton.setText("Participate");
-					participateButton.setBackgroundResource(R.drawable.button_background);
+					participateButton
+							.setBackgroundResource(R.drawable.button_background);
 				}
 			}
 		});
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case android.R.id.home:
 			onBackPressed();
-			// Intent intent = new Intent(getApplicationContext(), FlashmobDetailsActivity.class);
+			// Intent intent = new Intent(getApplicationContext(),
+			// FlashmobDetailsActivity.class);
 			// intent.putExtra("id", id);
 			// startActivity(intent);
 			return true;
@@ -112,8 +123,7 @@ public class ParticipateActivity extends SherlockActivity
 	 * 
 	 * @return
 	 */
-	public String getFlashmobID()
-	{
+	public String getFlashmobID() {
 		// Identify the flashmob
 		Bundle extras = getIntent().getExtras();
 
@@ -123,8 +133,7 @@ public class ParticipateActivity extends SherlockActivity
 		return theID;
 	}
 
-	public void getFlashmobData()
-	{
+	public void getFlashmobData() {
 		// Get the flashmob
 		flashmob = ((Store) getApplicationContext()).getFlashmobById(id);
 	}
