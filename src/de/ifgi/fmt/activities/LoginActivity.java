c2791e35787a83b4cc1785de47f3308f793890a4
Matthias Robbers <matthias.robbers@gmail.com>
2012-06-23 16:19:48 +0000

    Login working, Cookie saved in SharedPreferences, test output in StartActivity, user Id not real yet
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@166 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/de/ifgi/fmt/activities/LoginActivity.java
+++ src/de/ifgi/fmt/activities/LoginActivity.java
@@ -2,20 +2,26 @@ package de.ifgi.fmt.activities;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
-import android.app.AlertDialog;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Base64;
-import android.util.Log;
 import android.preference.PreferenceManager;
 import android.text.InputType;
+import android.util.Base64;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,23 +32,17 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.HttpEntity;
-
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 import de.ifgi.fmt.R;
+import de.ifgi.fmt.network.NetworkRequest;
 
-public class LoginActivity extends SherlockActivity
-{
+public class LoginActivity extends SherlockActivity {
 	public static final int STATUS_NOT_VALID = 12;
 	public static final int STATUS_OK = 11;
+
+	public static final int INVALID_CREDENTIALS = 11;
 
 	public static final int REDIRECT_TO_START_ACTIVITY = 1;
 	public static final int REDIRECT_TO_MY_FLASHMOBS_ACTIVITY = 2;
@@ -50,12 +50,10 @@ public class LoginActivity extends SherlockActivity
 
 	private EditText username, password;
 	private Button login;
-	private ProgressDialog progressDialog;
 	private String userpassEncoded;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
 		setTitle("Login");
@@ -65,12 +63,10 @@ public class LoginActivity extends SherlockActivity
 		// already in XML, specially for HTC in Java
 		username.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
 		password = (EditText) findViewById(R.id.password);
-		password.setOnEditorActionListener(new OnEditorActionListener()
-		{
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-			{
-				if (actionId == 0)
-				{
+		password.setOnEditorActionListener(new OnEditorActionListener() {
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				if (actionId == 0) {
 					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 					imm.hideSoftInputFromWindow(password.getWindowToken(), 0);
 					login.performClick();
@@ -80,122 +76,112 @@ public class LoginActivity extends SherlockActivity
 		});
 
 		login = (Button) findViewById(R.id.login);
-		login.setOnClickListener(new OnClickListener()
-		{
-			public void onClick(View v)
-			{
+		login.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
 				authenticate();
 			}
 		});
-
-		progressDialog = new ProgressDialog(this);
-		progressDialog.setMessage("Authenticating...");
 	}
 
-	private void authenticate()
-	{
-		if (username.getText().toString().equals("") || password.getText().toString().equals(""))
-		{
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setTitle("Missing data");
-			builder.setIcon(android.R.drawable.ic_dialog_alert);
-			builder.setMessage("Please enter your user name and password.").setNeutralButton("OK",
-					new DialogInterface.OnClickListener()
-					{
-						public void onClick(DialogInterface dialog, int which)
-						{
-							dialog.cancel();
-						}
-					});
-			builder.create().show();
+	private void authenticate() {
+		if (username.getText().toString().equals("")
+				|| password.getText().toString().equals("")) {
+			Toast.makeText(getApplicationContext(),
+					"Please enter user name and password.", Toast.LENGTH_LONG)
+					.show();
+		} else {
+			new LoginTask(this).execute();
 		}
-		else
-		{
+	}
+
+	class LoginTask extends AsyncTask<String, Void, Integer> {
+		ProgressDialog progressDialog;
+
+		public LoginTask(Context context) {
+			progressDialog = new ProgressDialog(context);
+			progressDialog.setMessage("Authenticating...");
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
 			progressDialog.show();
-			new Thread()
-			{
-				public void run()
-				{
-
-					// Login request (server) goes here...
-					try
-					{
-
-						userpassEncoded = Base64.encodeToString(
-								(username.getText() + ":" + password.getText()).getBytes("UTF-8"),
-								Base64.NO_WRAP);
-						//
-						// NetworkRequest request = new
-						// NetworkRequest("http://giv-flashmob.uni-muenster.de/fmt?authentication:basic "
-						// + userpassEncoded);
-						//
-						// request.send();
-						// Log.d("FMT Login", userpassEncoded);
-						// Log.d("FMT Login", request.getUrl());
-
-						HttpClient client = new DefaultHttpClient();
-						String getURL = "http://giv-flashmob.uni-muenster.de/fmt/";
-						HttpGet get = new HttpGet(getURL);
-						get.setHeader("Authorization", "Basic " + userpassEncoded);
-						HttpResponse responseGet = client.execute(get);
-						HttpEntity resEntityGet = responseGet.getEntity();
-						if (resEntityGet != null)
-						{
-							String response = EntityUtils.toString(resEntityGet);
-							Log.i("GET RESPONSE", response);
-						}
-
-					}
-					catch (UnsupportedEncodingException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					catch (ClientProtocolException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					catch (IOException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					handler.sendEmptyMessage(0);
-
-				};
-
-				private Handler handler = new Handler()
-				{
-					@Override
-					public void handleMessage(Message msg)
-					{
-						// work with the result
-						switch (msg.what)
-						{
-						default:
-							SharedPreferences preferences = PreferenceManager
-									.getDefaultSharedPreferences(getApplicationContext());
-							SharedPreferences.Editor editor = preferences.edit();
-							editor.putString("user_id", "4fe09086e4b0662f088a4fa5");
-							editor.putString("user_name", "markymark");
-							editor.putString("user_email", "wahlberg@hollywood.com");
-							editor.commit();
-							break;
-						}
-						progressDialog.dismiss();
-						redirect();
-					}
-				};
-			}.start();
 		}
+
+		@Override
+		protected Integer doInBackground(String... url) {
+			try {
+				userpassEncoded = Base64.encodeToString((username.getText()
+						+ ":" + password.getText()).getBytes("UTF-8"),
+						Base64.NO_WRAP);
+				DefaultHttpClient client = new DefaultHttpClient();
+				String getURL = "http://giv-flashmob.uni-muenster.de/fmt/";
+				HttpGet get = new HttpGet(getURL);
+				get.setHeader("Authorization", "Basic " + userpassEncoded);
+				HttpResponse responseGet = client.execute(get);
+				HttpEntity resEntityGet = responseGet.getEntity();
+				Log.i("wichtig", "Status: " + responseGet.getStatusLine());
+				if (responseGet.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+					return INVALID_CREDENTIALS;
+				}
+				if (resEntityGet != null) {
+					resEntityGet.consumeContent();
+					Log.i("wichtig", "Initial set of cookies:");
+					List<Cookie> cookies = client.getCookieStore().getCookies();
+					if (!cookies.isEmpty()) {
+						SharedPreferences preferences = PreferenceManager
+								.getDefaultSharedPreferences(getApplicationContext());
+						SharedPreferences.Editor editor = preferences.edit();
+						editor.putString("user_id", "4fe09086e4b0662f088a4fa5");
+						editor.putString("user_name", username.getText()
+								.toString());
+						for (int i = 0; i < cookies.size(); i++) {
+							editor.putString(cookies.get(i).getName(), cookies
+									.get(i).getValue());
+							Log.i("wichtig", "- " + cookies.get(i).toString());
+						}
+						editor.commit();
+					}
+					return 1;
+				}
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return NetworkRequest.NETWORK_PROBLEM;
+			}
+			return 0;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			super.onPostExecute(result);
+			switch (result) {
+			case NetworkRequest.NETWORK_PROBLEM:
+				Toast.makeText(getApplicationContext(),
+						"There is a problem with the Internet connection.",
+						Toast.LENGTH_LONG).show();
+				break;
+			case INVALID_CREDENTIALS:
+				Toast.makeText(getApplicationContext(),
+						"Username and password do not match.",
+						Toast.LENGTH_LONG).show();
+				password.setText("");
+				break;
+			default:
+				redirect();
+				break;
+			}
+			progressDialog.dismiss();
+		}
+
 	}
 
-	public void redirect()
-	{
+	public void redirect() {
 		Intent intent = null;
-		switch (getIntent().getExtras().getInt("startActivity"))
-		{
+		switch (getIntent().getExtras().getInt("startActivity")) {
 		case REDIRECT_TO_START_ACTIVITY:
 			intent = new Intent(this, StartActivity.class);
 			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -206,15 +192,14 @@ public class LoginActivity extends SherlockActivity
 			break;
 		}
 		startActivity(intent);
-		Toast.makeText(this, "Welcome " + username.getText(), Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "Welcome " + username.getText(), Toast.LENGTH_LONG)
+				.show();
 		finish();
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case android.R.id.home:
 			// app icon in action bar clicked; go home
 			Intent intent = new Intent(this, StartActivity.class);
--- src/de/ifgi/fmt/activities/StartActivity.java
+++ src/de/ifgi/fmt/activities/StartActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 
 import com.actionbarsherlock.app.SherlockActivity;
@@ -13,75 +14,68 @@ import com.actionbarsherlock.view.MenuItem;
 
 import de.ifgi.fmt.R;
 
-public class StartActivity extends SherlockActivity
-{
+public class StartActivity extends SherlockActivity {
 	private static final int MENU_WEBSITE = 1;
 	private static final int MENU_LOGIN = 2;
 	private static final int MENU_LOGOUT = 3;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_activity);
 	}
 
 	@Override
-	protected void onResume()
-	{
+	protected void onResume() {
 		super.onResume();
 		invalidateOptionsMenu();
+
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		Log.i("wichtig", "User ID: " + preferences.getString("user_id", ""));
+		Log.i("wichtig", "Cookie: " + preferences.getString("fmt_oid", ""));
 	}
 
-	public void startMapActivity(View v)
-	{
+	public void startMapActivity(View v) {
 		startActivity(new Intent(this, MapActivity.class));
 	}
 
-	public void startLocationActivity(View v)
-	{
+	public void startLocationActivity(View v) {
 		startActivity(new Intent(this, LocationActivity.class));
 	}
 
-	public void startAttributesActivity(View v)
-	{
+	public void startAttributesActivity(View v) {
 		startActivity(new Intent(this, AttributesActivity.class));
 	}
 
-	public void startMyFlashmobsActivity(View v)
-	{
+	public void startMyFlashmobsActivity(View v) {
 		SharedPreferences preferences = PreferenceManager
 				.getDefaultSharedPreferences(getApplicationContext());
-		if (preferences.getString("user_id", null) == null)
-		{
-			startActivity(new Intent(this, LoginActivity.class).putExtra("startActivity",
+		if (preferences.getString("user_id", null) == null) {
+			startActivity(new Intent(this, LoginActivity.class).putExtra(
+					"startActivity",
 					LoginActivity.REDIRECT_TO_MY_FLASHMOBS_ACTIVITY));
-		}
-		else
-		{
+		} else {
 			startActivity(new Intent(this, MyFlashmobsActivity.class));
 		}
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case MENU_WEBSITE:
-			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com")));
+			startActivity(new Intent(Intent.ACTION_VIEW,
+					Uri.parse("http://www.google.com")));
 			break;
 		case MENU_LOGIN:
-			startActivity(new Intent(this, LoginActivity.class).putExtra("startActivity",
-					LoginActivity.REDIRECT_TO_START_ACTIVITY));
+			startActivity(new Intent(this, LoginActivity.class).putExtra(
+					"startActivity", LoginActivity.REDIRECT_TO_START_ACTIVITY));
 			break;
 		case MENU_LOGOUT:
 			SharedPreferences preferences = PreferenceManager
 					.getDefaultSharedPreferences(getApplicationContext());
 			SharedPreferences.Editor editor = preferences.edit();
-			editor.remove("user_id");
-			editor.remove("user_name");
-			editor.remove("user_email");
+			editor.clear();
 			editor.commit();
 			invalidateOptionsMenu();
 		default:
@@ -91,24 +85,24 @@ public class StartActivity extends SherlockActivity
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		SharedPreferences preferences = PreferenceManager
 				.getDefaultSharedPreferences(getApplicationContext());
 		menu.add(0, MENU_WEBSITE, 0, "Website")
 				.setIcon(R.drawable.ic_action_website)
-				.setShowAsAction(MenuItem.SHOW_AS_ACTION_WITH_TEXT | MenuItem.SHOW_AS_ACTION_ALWAYS);
-		if (preferences.getString("user_id", null) == null)
-		{
+				.setShowAsAction(
+						MenuItem.SHOW_AS_ACTION_WITH_TEXT
+								| MenuItem.SHOW_AS_ACTION_ALWAYS);
+		if (preferences.getString("user_id", null) == null) {
 			menu.add(0, MENU_LOGIN, 0, "Login")
 					.setIcon(R.drawable.ic_action_login)
 					.setShowAsAction(
-							MenuItem.SHOW_AS_ACTION_WITH_TEXT | MenuItem.SHOW_AS_ACTION_ALWAYS);
-		}
-		else
-		{
+							MenuItem.SHOW_AS_ACTION_WITH_TEXT
+									| MenuItem.SHOW_AS_ACTION_ALWAYS);
+		} else {
 			menu.add(0, MENU_LOGOUT, 0, "Logout").setShowAsAction(
-					MenuItem.SHOW_AS_ACTION_WITH_TEXT | MenuItem.SHOW_AS_ACTION_ALWAYS);
+					MenuItem.SHOW_AS_ACTION_WITH_TEXT
+							| MenuItem.SHOW_AS_ACTION_ALWAYS);
 		}
 		return super.onCreateOptionsMenu(menu);
 	}
