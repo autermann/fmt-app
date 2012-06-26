a56bceadba77c866dcaa3445eab55d726b4ad3e4
Matthias Robbers <matthias.robbers@gmail.com>
2012-06-26 15:31:55 +0000

    classes for activity, task, ...
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@190 471a90e3-85d6-13bc-70e2-46c3232741f2

---
+++ src/de/ifgi/fmt/objects/Trigger.java
@@ -0,0 +1,38 @@
+package de.ifgi.fmt.objects;
+
+import java.util.Date;
+
+import android.location.Location;
+
+public class Trigger {
+	private String id;
+	private String description;
+	private Location location;
+	private Date time;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	
+	
+}
