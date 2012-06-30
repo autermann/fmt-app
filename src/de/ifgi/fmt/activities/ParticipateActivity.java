d2fcfa4eacdfd19ad743c4efeb01ad1ca2bc8237
Matthias Robbers <matthias.robbers@gmail.com>
2012-06-30 15:14:47 +0000

    ParticipateActivity
    git-svn-id: https://fmt.googlecode.com/svn/App/trunk@218 471a90e3-85d6-13bc-70e2-46c3232741f2

--- src/de/ifgi/fmt/activities/ParticipateActivity.java
+++ src/de/ifgi/fmt/activities/ParticipateActivity.java
@@ -4,19 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.ClientContext;
-import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,6 +19,7 @@ import org.json.JSONObject;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -41,7 +37,6 @@ import com.actionbarsherlock.view.MenuItem;
 import de.ifgi.fmt.R;
 import de.ifgi.fmt.adapter.RolesSpinnerAdapter;
 import de.ifgi.fmt.data.Store;
-import de.ifgi.fmt.network.NetworkRequest;
 import de.ifgi.fmt.objects.Flashmob;
 import de.ifgi.fmt.objects.Role;
 import de.ifgi.fmt.parser.RoleJSONParser;
@@ -50,9 +45,6 @@ public class ParticipateActivity extends SherlockActivity {
 	private Button participateButton;
 	private SharedPreferences prefs;
 	private Flashmob flashmob;
-	private String fId;
-	private String PARTICIPATION_PREF_KEY;
-	private String ROLE_ID_PREF_KEY;
 	private Spinner roleSpinner;
 	private ArrayList<Role> roles;
 	private TextView roleDescriptionTv;
@@ -67,29 +59,25 @@ public class ParticipateActivity extends SherlockActivity {
 
 		getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		// Initialize id
-		fId = getFlashmobID();
-
-		// Key string for saving the user's participation status
-		PARTICIPATION_PREF_KEY = fId + "ParticipationPref";
-
-		// Key string for saving selected roleId
-		ROLE_ID_PREF_KEY = fId + "RoleIdPref";
+		Store store = (Store) getApplicationContext();
+		// Initialize flashmob
+		flashmob = store.getFlashmobById(getIntent().getExtras()
+				.getString("id"));
+		setTitle(flashmob.getTitle());
 
 		// Initialize SharedPreferences
 		prefs = SettingsActivity.getSettings(this);
 
 		// Get participation status
-		isParticipating = prefs.getBoolean(PARTICIPATION_PREF_KEY, false);
+		if (flashmob.getSelectedRole() != null)
+			isParticipating = true;
+		else
+			isParticipating = false;
 
 		// Setting selectedRoleId to the roleId that the user registered for.
 		// Else set it as null (important!).
-		selectedRoleId = prefs.getString(ROLE_ID_PREF_KEY, null);
-
-		// Get the flashmob data
-		getFlashmobData();
-
-		setTitle(flashmob.getTitle());
+		if (isParticipating)
+			selectedRoleId = flashmob.getSelectedRole().getId();
 
 		// Participate button
 		participateButton = (Button) findViewById(R.id.participateButton);
@@ -99,43 +87,29 @@ public class ParticipateActivity extends SherlockActivity {
 			public void onClick(View v) {
 				if (!isParticipating) {
 					// Register a user for a role
-
-					// String selectedItem = ((RolesSpinnerAdapter)
-					// roleSpinner.getAdapter()).getItem(
-					// roleSpinner.getSelectedItemPosition()).getId();
-					// String url =
-					// "http://giv-flashmob.uni-muenster.de/fmt/flashmobs/" +
-					// fId
-					// + "/roles/" + selectedItem + "/users";
-
+					String selectedItem = ((RolesSpinnerAdapter) roleSpinner
+							.getAdapter()).getItem(
+							roleSpinner.getSelectedItemPosition()).getId();
 					String url = "http://giv-flashmob.uni-muenster.de/fmt/flashmobs/"
-							+ fId + "/roles/" + selectedRoleId + "/users";
+							+ flashmob.getId()
+							+ "/roles/"
+							+ selectedItem
+							+ "/users";
 					new UploadTask(ParticipateActivity.this).execute(url);
 				} else {
-					// TODO: Add Cancel-Participate-Funktion
 					// Unregister a user for a role
-					// Wichtig: Rolle canceln, die in SharedPrefs gespeichert
-					// ist
-					// String selectedItem = ((RolesSpinnerAdapter)
-					// roleSpinner.getAdapter()).getItem(
-					// roleSpinner.getSelectedItemPosition()).getId();
-					// String url =
-					// "http://giv-flashmob.uni-muenster.de/fmt/flashmobs/" +
-					// fId
-					// + "/roles/" + selectedItem + "/users/" + selectedItem;
 					String url = "http://giv-flashmob.uni-muenster.de/fmt/flashmobs/"
-							+ fId
+							+ flashmob.getId()
 							+ "/roles/"
 							+ selectedRoleId
-							+ "/users/"
-							+ prefs.getString("user_name", "");
+							+ "/users/" + prefs.getString("user_name", "");
 					new CancelTask(ParticipateActivity.this).execute(url);
 				}
 			}
 		});
 
-		String url = "http://giv-flashmob.uni-muenster.de/fmt/flashmobs/" + fId
-				+ "/roles";
+		String url = "http://giv-flashmob.uni-muenster.de/fmt/flashmobs/"
+				+ flashmob.getId() + "/roles";
 		new DownloadTask(this).execute(url);
 	}
 
@@ -143,39 +117,14 @@ public class ParticipateActivity extends SherlockActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			onBackPressed();
-			// Intent intent = new Intent(getApplicationContext(),
-			// FlashmobDetailsActivity.class);
-			// intent.putExtra("id", id);
-			// startActivity(intent);
+			Intent intent = new Intent(getApplicationContext(),
+					FlashmobDetailsActivity.class);
+			startActivity(intent);
 			return true;
 
 		default:
 			return super.onOptionsItemSelected(item);
 		}
-	}
-
-	/**
-	 * Gets the flashmob's id from the intent that is starting this Activity.
-	 * 
-	 * @return
-	 */
-	public String getFlashmobID() {
-		// Identify the flashmob
-		Bundle extras = getIntent().getExtras();
-
-		// Get the ID
-		String theID = extras.getString("id");
-
-		return theID;
-	}
-
-	/**
-	 * Initializes the flashmob with its data.
-	 */
-	public void getFlashmobData() {
-		// Get the flashmob
-		flashmob = ((Store) getApplicationContext()).getFlashmobById(fId);
 	}
 
 	/**
@@ -187,18 +136,16 @@ public class ParticipateActivity extends SherlockActivity {
 	public int spinnerPos() {
 		// If a role has been selected for this flashmob, get its Id
 		// Otherwise set it to the Id of the first Role in the list of roles
-		String tempRoleId = prefs.getString(ROLE_ID_PREF_KEY, roles.get(0)
-				.getId());
-
 		int spinnerPos = 0;
 
-		for (int i = 0; i < roles.size(); i++) {
-			if (roles.get(i).getId().equals(tempRoleId)) {
-				spinnerPos = i;
-				break;
+		if (isParticipating && selectedRoleId != null) {
+			for (int i = 0; i < roles.size(); i++) {
+				if (roles.get(i).getId().equals(selectedRoleId)) {
+					spinnerPos = i;
+					break;
+				}
 			}
 		}
-
 		return spinnerPos;
 	}
 
@@ -219,7 +166,6 @@ public class ParticipateActivity extends SherlockActivity {
 	}
 
 	class DownloadTask extends AsyncTask<String, Void, ArrayList<String>> {
-		// Is shown when the activity starts and while downloading the roles
 		ProgressDialog progressDialog;
 
 		public DownloadTask(Context context) {
@@ -235,20 +181,26 @@ public class ParticipateActivity extends SherlockActivity {
 
 		@Override
 		protected ArrayList<String> doInBackground(String... url) {
-			// Request for getting all roleIds
-			NetworkRequest request = new NetworkRequest(url[0]);
+			try {
+				// Request for getting all roleIds
+				HttpClient client = new DefaultHttpClient();
+				HttpGet request = new HttpGet(url[0]);
+				HttpResponse response;
 
-			// roleIds
-			ArrayList<String> roleIds = new ArrayList<String>();
+				response = client.execute(request);
+				String result = EntityUtils.toString(response.getEntity());
 
-			int result = request.send();
+				Log.i("wichtig", "URL: " + request.getURI());
+				Log.i("wichtig", "Status: " + response.getStatusLine());
+				Log.i("wichtig", "Result: " + result);
 
-			// Getting all roleIds
-			if (result == NetworkRequest.NETWORK_PROBLEM) {
-				return null;
-			} else {
+				// roleIds
+				ArrayList<String> roleIds = new ArrayList<String>();
+
+				// Getting all roleIds
+
 				try {
-					JSONObject root = new JSONObject(request.getResult());
+					JSONObject root = new JSONObject(result);
 					JSONArray roles = root.getJSONArray("roles");
 
 					for (int i = 0; i < roles.length(); i++) {
@@ -257,27 +209,28 @@ public class ParticipateActivity extends SherlockActivity {
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
-			}
 
-			// Roles
-			ArrayList<String> results = new ArrayList<String>();
+				// Roles
+				ArrayList<String> results = new ArrayList<String>();
 
-			// Getting all roles
-			for (String rId : roleIds) {
-				// Request for every single roleId
-				request = new NetworkRequest(
-						"http://giv-flashmob.uni-muenster.de/fmt/flashmobs/"
-								+ fId + "/roles" + "/" + rId);
-				result = request.send();
-
-				if (result == NetworkRequest.NETWORK_PROBLEM) {
-					return null;
-				} else {
-					results.add(request.getResult());
+				// Getting all roles
+				for (String rId : roleIds) {
+					// Request for every single roleId
+					request = new HttpGet(
+							"http://giv-flashmob.uni-muenster.de/fmt/flashmobs/"
+									+ flashmob.getId() + "/roles" + "/" + rId);
+					response = client.execute(request);
+					result = EntityUtils.toString(response.getEntity());
+					results.add(result);
 				}
+				return results;
+			} catch (ClientProtocolException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				return null;
 			}
-
-			return results;
+			return null;
 		}
 
 		@Override
@@ -326,9 +279,6 @@ public class ParticipateActivity extends SherlockActivity {
 										int pos, long id) {
 									Role r = (Role) parent
 											.getItemAtPosition(pos);
-									// Toast.makeText(getApplicationContext(),
-									// "Role ID: " + r.getId(),
-									// Toast.LENGTH_LONG).show();
 
 									// Set the TextViews with the selected
 									// role's
@@ -336,16 +286,11 @@ public class ParticipateActivity extends SherlockActivity {
 									roleDescriptionTv.setText(r
 											.getDescription());
 									roleItemsTv.setText(r.returnItemsAsString());
-
-									// Save roleId to "remember" the selected
-									// role
-									selectedRoleId = r.getId();
 								}
 
 								@Override
 								public void onNothingSelected(
 										AdapterView<?> arg0) {
-									// Do nothing
 								}
 							});
 				}
@@ -363,8 +308,6 @@ public class ParticipateActivity extends SherlockActivity {
 	}
 
 	class UploadTask extends AsyncTask<String, Void, Integer> {
-
-		// Is shown while the activity is uploading the participation data
 		ProgressDialog progressDialog;
 
 		public UploadTask(Context context) {
@@ -385,85 +328,55 @@ public class ParticipateActivity extends SherlockActivity {
 			String jsonString = "{\"username\":\""
 					+ prefs.getString("user_name", "") + "\"}";
 
-			// Create a local instance of cookie store
-			CookieStore cookieStore = new BasicCookieStore();
-
-			// Create cookie from SharedPrefs and add it to the cookie store
-			String name = "fmt_oid";
-			String value = prefs.getString("fmt_oid", "");
-			Cookie cookie = new BasicClientCookie(name, value);
-			cookieStore.addCookie(cookie);
-
-			// Create local HTTP context
-			HttpContext localContext = new BasicHttpContext();
-
-			// Bind cookie store to the local context
-			localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
-
 			// HTTP POST Request to Server to register a user for a role
 			HttpClient httpclient = new DefaultHttpClient();
 			HttpPost httppost = new HttpPost(url[0]);
 			httppost.setHeader("Content-Type", "application/json");
 
-			Log.d("wichtig", "URL: " + url[0]);
+			// Get cookie from SharedPrefs and add it to the header
+			String name = "fmt_oid";
+			String value = prefs.getString("fmt_oid", "");
+			httppost.setHeader("Cookie", name + "=" + value);
+
+			Log.i("wichtig", "URL: " + httppost.getURI());
 
 			try {
-
 				httppost.setEntity(new StringEntity(jsonString));
-
-				HttpResponse response = httpclient.execute(httppost,
-						localContext);
+				HttpResponse response = httpclient.execute(httppost);
 				Log.i("wichtig", "Status: " + response.getStatusLine());
 				Log.i("wichtig",
 						"Response: "
 								+ EntityUtils.toString(response.getEntity()));
 				return response.getStatusLine().getStatusCode();
 			} catch (ClientProtocolException e) {
-
 				e.printStackTrace();
-
 			} catch (IOException e) {
-
 				e.printStackTrace();
-
 				return 0;
 			}
 			return 0;
-
 		}
 
 		@Override
 		protected void onPostExecute(Integer result) {
-			final SharedPreferences.Editor editor = prefs.edit();
-
 			super.onPostExecute(result);
-			if (result == 201) {
+			if (result == HttpStatus.SC_CREATED) {
+				isParticipating = true;
 				// Change layout of the Participate/Cancel-Button
 				participateButton.setText("Cancel Participation");
 				participateButton
 						.setBackgroundResource(R.drawable.cancel_button_background);
-
-				// Save participation status in SharedPrefs
-				editor.putBoolean(PARTICIPATION_PREF_KEY, true);
-				editor.commit();
-
-				// Save roleId in SharedPreferences
-				editor.putString(ROLE_ID_PREF_KEY, selectedRoleId);
-				editor.commit();
 			} else if (result == 0) {
 				Toast.makeText(getApplicationContext(),
 						"There is a problem with the Internet connection.",
 						Toast.LENGTH_LONG).show();
 			}
-
 			progressDialog.dismiss();
 		}
 
 	}
 
 	class CancelTask extends AsyncTask<String, Void, Integer> {
-
-		// Is shown while the activity is uploading the participation data
 		ProgressDialog progressDialog;
 
 		public CancelTask(Context context) {
@@ -496,13 +409,9 @@ public class ParticipateActivity extends SherlockActivity {
 
 				return response.getStatusLine().getStatusCode();
 			} catch (ClientProtocolException e) {
-
 				e.printStackTrace();
-
 			} catch (IOException e) {
-
 				e.printStackTrace();
-
 				return 0;
 			}
 			return 0;
@@ -510,24 +419,18 @@ public class ParticipateActivity extends SherlockActivity {
 
 		@Override
 		protected void onPostExecute(Integer result) {
-			final SharedPreferences.Editor editor = prefs.edit();
-
 			super.onPostExecute(result);
-			if (result == 204) {
+			if (result == HttpStatus.SC_NO_CONTENT) {
+				isParticipating = false;
 				// Change layout of the Participate/Cancel-Button
 				participateButton.setText("Participate");
 				participateButton
 						.setBackgroundResource(R.drawable.button_background);
-
-				// Save participation status in SharedPrefs
-				editor.putBoolean(PARTICIPATION_PREF_KEY, false);
-				editor.commit();
 			} else if (result == 0) {
 				Toast.makeText(getApplicationContext(),
 						"There is a problem with the Internet connection.",
 						Toast.LENGTH_LONG).show();
 			}
-
 			progressDialog.dismiss();
 		}
 
